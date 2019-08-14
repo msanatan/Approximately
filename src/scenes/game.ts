38151aefab964150e4b9882b7e502576df67e0ad
Marcus Sanatan <msanatan@gmail.com>
2019-08-14 08:36:02 -0400

    Add a divider and a ball

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -13,7 +13,28 @@ export class GameScene extends Phaser.Scene {
 
   init(): void {}
 
-  create(): void {}
+  create(): void {
+    this.cameras.main.setBackgroundColor('#38B6FF');
+    const separator = new Phaser.Geom.Line(this.game.canvas.width, 0,
+      0, this.game.canvas.height);
+    const separatorGraphics = this.add.graphics({
+      lineStyle: {
+        width: 5,
+        color: 0x000000
+      }
+    });
+    separatorGraphics.strokeLineShape(separator);
+
+    // Create yellow ball for player
+    const playerBall = new Phaser.Geom.Circle(600, 400, 30);
+    const playerGraphics = this.add.graphics({
+      fillStyle: {
+        alpha: 1,
+        color: 0xffd700
+      }
+    });
+    playerGraphics.fillCircleShape(playerBall);
+  }
 
   update(time: number, delta: number): void {}
 }
