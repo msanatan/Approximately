236288155fa38f015c4d8a54945d490b531140eb
Marcus Sanatan <msanatan@gmail.com>
2019-08-21 23:50:02 -0400

    Minor position tweaks

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -21,10 +21,10 @@ export class GameScene extends Phaser.Scene {
     let width = this.game.canvas.width;
     let height = this.game.canvas.height;
     this.cameras.main.setBackgroundColor('#38B6FF');
-    this.separatorLine = this.physics.add.sprite(width / 2, height / 2, 'separator');
+    this.separatorLine = this.physics.add.sprite(width / 2, (height / 2) - 5, 'separator');
     this.separatorLine.setImmovable(true);
     this.separatorLine.body.setAllowGravity(false);
-    this.playerBall = this.physics.add.sprite(600, 400, 'playerBall');
+    this.playerBall = this.physics.add.sprite(200, 400, 'playerBall');
     this.playerBall.setBounce(0.4);
     this.playerBall.setCollideWorldBounds(true);
   }
