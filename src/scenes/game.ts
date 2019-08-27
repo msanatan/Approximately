350248e6814f7888387994c06584268eb414a4e4
Marcus Sanatan <msanatan@gmail.com>
2019-08-26 22:07:17 -0400

    Use the grow button to scale the player up to 10 times
    We also scale down the player ball to make things more interesting.

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -9,7 +9,9 @@ import 'phaser';
 export class GameScene extends Phaser.Scene {
   private playerBall : Phaser.Physics.Arcade.Sprite;
   private separatorLine : Phaser.Physics.Arcade.Sprite;
-  // private guessBalls: Phaser.GameObjects.Group;
+  private guessBalls : Phaser.Physics.Arcade.Group;
+  private growButton : Phaser.GameObjects.Sprite;
+  private growCount : number;
 
   constructor() {
     super({ key: 'GameScene' });
@@ -24,10 +26,23 @@ export class GameScene extends Phaser.Scene {
     this.separatorLine = this.physics.add.sprite(width / 2, (height / 2) - 5, 'separator');
     this.separatorLine.setImmovable(true);
     this.separatorLine.body.setAllowGravity(false);
-    this.playerBall = this.physics.add.sprite(200, 400, 'playerBall');
+    this.playerBall = this.physics.add.sprite(200, 400, 'playerBall').setScale(0.5);
     this.playerBall.setBounce(0.4);
     this.playerBall.setCollideWorldBounds(true);
+
+    this.growCount = 0;
+    // Add button to increase ball size
+    this.growButton = this.add.sprite(700, 400, 'growButton');
+    this.growButton.setInteractive();
+    this.growButton.on('pointerdown', () => this.scalePlayerBall());
   }
 
   update(time: number, delta: number): void {}
+
+  scalePlayerBall() {
+    if (this.growCount < 10) {
+      this.growCount++;
+      this.playerBall.setScale(this.playerBall.scaleX + 0.2);
+    }
+  }
 }
