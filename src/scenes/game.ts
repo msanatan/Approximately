e799ee0d53ae9ddae30a7ebce3f3aa2257e3b758
Marcus Sanatan <msanatan@gmail.com>
2019-09-03 00:30:24 -0400

    Check to see if level was cleared
    Lots of things going on here:
    
    * We load google fonts
    * We have a function that checks if the player guessed correctly
    * A couple of text messages for win/lose
    * Remove `repeat` from early level sprite groups

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -11,8 +11,12 @@ export class GameScene extends Phaser.Scene {
   private separatorLine : Phaser.Physics.Arcade.Sprite;
   private guessBalls : Phaser.Physics.Arcade.Group;
   private growButton : Phaser.GameObjects.Sprite;
+  private checkButton : Phaser.GameObjects.Sprite;
   private growCount : number;
   private level : number;
+  private levelText : Phaser.GameObjects.Text;
+  private levelCompleteText : Phaser.GameObjects.Text;
+  private lostLevelText : Phaser.GameObjects.Text;
 
   constructor() {
     super({ key: 'GameScene' });
@@ -31,20 +35,34 @@ export class GameScene extends Phaser.Scene {
     this.playerBall.setBounce(0.4);
     this.playerBall.setCollideWorldBounds(true);
 
+    // Set up level
+    this.level = 1;
+    this.getLevel();
+    this.physics.add.collider(this.separatorLine, this.guessBalls);
+
     this.growCount = 0;
     // Add button to increase ball size
     this.growButton = this.add.sprite(700, 400, 'growButton');
     this.growButton.setInteractive();
     this.growButton.on('pointerdown', () => this.scalePlayerBall());
 
-    // Add button to do comparison
-    this.growButton = this.add.sprite(700, 475, 'checkButton');
-    this.growButton.setInteractive();
+    // Add button to compare sizes
+    this.checkButton = this.add.sprite(700, 475, 'checkButton');
+    this.checkButton.setInteractive();
+    this.checkButton.on('pointerdown', () => this.compareBalls());
 
-    // Set up level
-    this.level = 1;
-    this.getLevel();
-    this.physics.add.collider(this.separatorLine, this.guessBalls);
+    WebFont.load({
+      google: {
+          families: ['Luckiest Guy']
+      },
+      active: () => {
+          this.levelText = this.add.text(20, 40, 'Level: ' + this.level, {
+            fontFamily: 'Luckiest Guy',
+            fontSize: 30,
+            color: '#ffffff'
+          });
+      }
+  });
   }
 
   update(time: number, delta: number): void {}
@@ -56,12 +74,42 @@ export class GameScene extends Phaser.Scene {
     }
   }
 
+  compareBalls() {
+    let totalGuessSize = 0;
+    this.guessBalls.getChildren().forEach((gb: Phaser.Physics.Arcade.Sprite) => {
+      totalGuessSize += gb.displayWidth;
+    });
+
+    if (Math.floor(totalGuessSize) === Math.floor(this.playerBall.displayWidth)) {
+      this.levelCompleteText = this.add.text(
+        this.physics.world.bounds.centerX,
+        this.physics.world.bounds.centerY + 40,
+        'Good Job!',
+        {
+          fontFamily: 'Luckiest Guy',
+          fontSize: 40,
+          color: '#ffffff'
+        }
+      ).setOrigin(0.5);
+    } else {
+      this.levelCompleteText = this.add.text(
+        this.physics.world.bounds.centerX,
+        this.physics.world.bounds.centerY + 40,
+        'You Lose :-(',
+        {
+          fontFamily: 'Luckiest Guy',
+          fontSize: 40,
+          color: '#ffffff'
+        }
+      ).setOrigin(0.5);
+    }
+  }
+
   getLevel() {
     switch(this.level) {
       case 1:
         this.guessBalls = this.physics.add.group({
           key: 'guessBall',
-          repeat: 1,
           setXY: {
             x: 200,
             y: 50,
@@ -77,7 +125,6 @@ export class GameScene extends Phaser.Scene {
       case 2:
           this.guessBalls = this.physics.add.group({
             key: 'guessBall',
-            repeat: 1,
             setXY: {
               x: 200,
               y: 50,
