3952ed1b91ffae8d4584348e2115cbd8b2497923
Marcus Sanatan <msanatan@gmail.com>
2019-09-02 22:49:52 -0400

    Add levels to game
    We can define how many balls we want to appear in a level

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -12,6 +12,7 @@ export class GameScene extends Phaser.Scene {
   private guessBalls : Phaser.Physics.Arcade.Group;
   private growButton : Phaser.GameObjects.Sprite;
   private growCount : number;
+  private level : number;
 
   constructor() {
     super({ key: 'GameScene' });
@@ -39,6 +40,11 @@ export class GameScene extends Phaser.Scene {
     // Add button to do comparison
     this.growButton = this.add.sprite(700, 475, 'checkButton');
     this.growButton.setInteractive();
+
+    // Set up level
+    this.level = 1;
+    this.getLevel();
+    this.physics.add.collider(this.separatorLine, this.guessBalls);
   }
 
   update(time: number, delta: number): void {}
@@ -47,6 +53,42 @@ export class GameScene extends Phaser.Scene {
     if (this.growCount < 10) {
       this.growCount++;
       this.playerBall.setScale(this.playerBall.scaleX + 0.2);
+    }
+  }
+
+  getLevel() {
+    switch(this.level) {
+      case 1:
+        this.guessBalls = this.physics.add.group({
+          key: 'guessBall',
+          repeat: 1,
+          setXY: {
+            x: 200,
+            y: 50,
+          },
+          bounceX: 0.4,
+          bounceY: 0.4,
+          setScale: {
+            x: 0.6,
+            y: 0.6
+          }
+        });
+        break;
+      case 2:
+          this.guessBalls = this.physics.add.group({
+            key: 'guessBall',
+            repeat: 1,
+            setXY: {
+              x: 200,
+              y: 50,
+            },
+            bounceX: 0.4,
+            bounceY: 0.4,
+            setScale: {
+              x: 1.2,
+              y: 1.2,
+            }
+          });
     }
   }
 }
