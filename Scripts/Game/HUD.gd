d05e09ce7937ae7253e47a3411599426c43444e0
Marcus Sanatan <msanatan@gmail.com>
2022-01-23 20:24:23 -0400

    Add type hints to HUD

--- Scripts/Game/HUD.gd
+++ Scripts/Game/HUD.gd
@@ -4,17 +4,17 @@ onready var next_answer_label = $CanvasLayer/RemainingTimeLabel
 onready var remaining_answer_label = $CanvasLayer/RemainingAnswersLabel
 
 
-func set_countdown_text(remaining_time):
+func set_countdown_text(remaining_time: int):
     next_answer_label.text = str(remaining_time)
 
 
-func set_remaining_tries_text(remaining_tries):
+func set_remaining_tries_text(remaining_tries: int):
     remaining_answer_label.text = 'Remaining Tries: ' + str(remaining_tries)
 
 
-func _on_Game_remaining_time_update(remaining_time):
+func _on_Game_remaining_time_update(remaining_time: int):
 	set_countdown_text(remaining_time)
 
 
-func _on_Game_remaining_tries_update(remaining_tries):
+func _on_Game_remaining_tries_update(remaining_tries: int):
 	set_remaining_tries_text(remaining_tries)
