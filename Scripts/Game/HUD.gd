9119cc3b7cb6a069d21cad7430f7cfc1d66f3677
Marcus Sanatan <msanatan@gmail.com>
2022-01-23 21:12:57 -0400

    Add a zero in countdown when number is less than 10
    Keeps things consistent

--- Scripts/Game/HUD.gd
+++ Scripts/Game/HUD.gd
@@ -5,7 +5,7 @@ onready var remaining_answer_label = $CanvasLayer/RemainingAnswersLabel
 
 
 func set_countdown_text(remaining_time: int):
-    next_answer_label.text = str(remaining_time)
+    next_answer_label.text = str(remaining_time) if remaining_time >= 10 else '0%d' % remaining_time
 
 
 func set_remaining_tries_text(remaining_tries: int):
@@ -13,8 +13,8 @@ func set_remaining_tries_text(remaining_tries: int):
 
 
 func _on_Game_remaining_time_update(remaining_time: int):
-	set_countdown_text(remaining_time)
+    set_countdown_text(remaining_time)
 
 
 func _on_Game_remaining_tries_update(remaining_tries: int):
-	set_remaining_tries_text(remaining_tries)
+    set_remaining_tries_text(remaining_tries)
