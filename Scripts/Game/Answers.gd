ef986c5d33f9249d58eb9dbe554e1d8c9e4ce50e
Marcus Sanatan <msanatan@gmail.com>
2022-02-05 16:43:50 -0400

    Wrong answers disappear when selected

--- Scripts/Game/Answers.gd
+++ Scripts/Game/Answers.gd
@@ -16,29 +16,34 @@ func set_answers(answers: Array, correct_ans: int):
     answer_2.text = str(answers[1])
     answer_3.text = str(answers[2])
     answer_4.text = str(answers[3])
+    answer_1.visible = true
+    answer_2.visible = true
+    answer_3.visible = true
+    answer_4.visible = true
     correct_answer = correct_ans
 
-func is_correct_answer(answer: int):
+func is_correct_answer(answer: int, node: Button):
     var is_correct: bool = answer == correct_answer
 
     if is_correct:
         emit_signal('right_answer_selected')
     else:
+        node.visible = false
         emit_signal('wrong_answer_selected')
 
     return is_correct
 
 func _on_Answer1_pressed():
-    is_correct_answer(int(answer_1.text))
+    is_correct_answer(int(answer_1.text), answer_1)
 
 
 func _on_Answer2_pressed():
-    is_correct_answer(int(answer_2.text))
+    is_correct_answer(int(answer_2.text), answer_2)
 
 
 func _on_Answer3_pressed():
-    is_correct_answer(int(answer_3.text))
+    is_correct_answer(int(answer_3.text), answer_3)
 
 
 func _on_Answer4_pressed():
-    is_correct_answer(int(answer_4.text))
+    is_correct_answer(int(answer_4.text), answer_4)
