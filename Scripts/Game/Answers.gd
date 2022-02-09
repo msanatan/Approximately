465855faac7bcbfdd43b3895e4dcc0fe22ca58b3
Marcus Sanatan <msanatan@gmail.com>
2022-02-08 23:29:17 -0400

    Add an animation player for the buttons to move
    I used Tweens, looks like more setup for multiple nodes.
    Works well!

--- Scripts/Game/Answers.gd
+++ Scripts/Game/Answers.gd
@@ -7,8 +7,12 @@ onready var answer_1: Button = $Answer1
 onready var answer_2: Button = $Answer2
 onready var answer_3: Button = $Answer3
 onready var answer_4: Button = $Answer4
+onready var button_animation: AnimationPlayer = $AnimationPlayer
 
 var correct_answer: int
+
+func _ready():
+    button_animation.play('ButtonMove')
 
 
 func set_answers(answers: Array, correct_ans: int):
