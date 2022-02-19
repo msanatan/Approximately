d09bc3f5dac33cc0bbd9e5b74ffce713de9df386
Marcus Sanatan <msanatan@gmail.com>
2022-02-19 16:57:29 -0400

    Change text/vars to total attempts + fix score bug
    The score didn't reset when playing again from the main menu

--- Scripts/GameOver.gd
+++ Scripts/GameOver.gd
@@ -1,32 +1,36 @@
 extends Node2D
 
-onready var total_questions_label: Label = $StatsContainer/TotalQuestionsLabel
+onready var total_attempts_label: Label = $StatsContainer/TotalAttemptsLabel
 onready var total_correct_label: Label = $StatsContainer/TotalCorrectLabel
 onready var total_wrong_label: Label = $StatsContainer/TotalWrongLabel
 onready var results_label: Label = $ResultsMessageLabel
 
 
 func _ready():
-    total_questions_label.text = 'Total Questions: ' + str(Global.total_questions)
+    total_attempts_label.text = 'Total Attemps: ' + str(Global.total_attempts)
     total_correct_label.text = 'Total Correct: ' + str(Global.total_correct)
     total_wrong_label.text = 'Total Wrong: ' + str(Global.total_wrong)
 
-    if Global.total_wrong == 0 and Global.total_questions >= 10:
+    if Global.total_wrong == 0 and Global.total_attempts >= 10:
         results_label.text = "You're acing it!\nKeep up the good work!"
-    elif Global.total_wrong == 0 and Global.total_questions < 10:
+    elif Global.total_wrong == 0 and Global.total_attempts < 10:
         results_label.text = "All correct!\nKeep it up and go for more!"
-    elif Global.total_correct >= Global.total_wrong and Global.total_questions >= 10:
+    elif Global.total_correct >= Global.total_wrong and Global.total_attempts >= 10:
         results_label.text = "Keep it up!\nYou're doing well, just take your time!"
-    elif Global.total_correct >= Global.total_wrong and Global.total_questions < 10:
+    elif Global.total_correct >= Global.total_wrong and Global.total_attempts < 10:
         results_label.text = "You're on the right path!\nTry to focus on accuracy!"
     elif Global.total_correct < Global.total_wrong:
         results_label.text = "Don't give up!\nIt's all about practice, you got this!"
 
 
-func _on_ReplayButton_pressed():
-    Global.total_questions = 0
+func reset_scores():
+    Global.total_attempts = 0
     Global.total_correct = 0
     Global.total_wrong = 0
+
+
+func _on_ReplayButton_pressed():
+    reset_scores()
     $CircleGrowingTransition.transition_start('#0f3daa')
 
 
@@ -35,6 +39,7 @@ func _on_CircleGrowingTransition_transition_finished():
 
 
 func _on_MainMenuButton_pressed():
+    reset_scores()
     $FadeTransition.show()
     $FadeTransition.fade_out()
 
--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,5 +1,5 @@
 extends Node
 
-var total_questions = 0
+var total_attempts = 0
 var total_correct = 0
 var total_wrong = 0
