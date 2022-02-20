a055dfecee8e52fa693a749a161a17c2051fa231
Marcus Sanatan <msanatan@gmail.com>
2022-02-20 00:03:34 -0400

    Add a button to toggle sound on/off
    Needs a little state management to ensure the sound off icons shows correctly.

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
 onready var circle: Sprite = $CircleSprite
+var volume_on = load('res://Assets/Images/UI/volume-up-4-512.png')
+var volume_off = load('res://Assets/Images/UI/mute-2-512.png')
+var sound_on: bool = true
 
 func _ready():
     $FadeTransition.fade_in()
+    if not MusicPlayer.playing:
+        $SoundButton/SoundIcon.set_texture(volume_off)
+        sound_on = false
 
 func _process(delta):
     circle.rotate(delta * PI * 2)
@@ -20,3 +26,15 @@ func _on_CircleGrowingTransition_transition_finished():
 
 func _on_FadeTransition_transition_finished():
     $FadeTransition.hide()
+
+
+func _on_SoundButton_pressed():
+    if sound_on:
+        MusicPlayer.stop()
+        $SoundButton/SoundIcon.set_texture(volume_off)
+    else:
+        MusicPlayer.play()
+        $SoundButton/SoundIcon.set_texture(volume_on)
+
+    $SoundButton/SoundIcon.modulate = '#0f3daa'
+    sound_on = !sound_on
